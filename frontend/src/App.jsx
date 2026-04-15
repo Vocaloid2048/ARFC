28cbc4a76dc6e52113be4dcdb502972cd1154dd6
Voc-夜芷冰 <voc.app.programmer@gmail.com>
2026-04-16 02:31:04 +0800

    feat: Footer
    其實我不想弄這麼亂...明天再努力吧最近感覺思考也很混亂...該休息多點了但作業等着我...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import HeaderActions from './components/HeaderActions'
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-[var(--page-bg)] font-sans">
+      <div className="min-h-screen flex flex-col bg-[var(--page-bg)] font-sans">
         <nav className="bg-[var(--nav-bg)] border-b border-slate-500 sticky top-0 z-50 shadow-sm">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between h-16">
@@ -23,13 +23,19 @@ function App() {
           </div>
         </nav>
         
-        <main>
+        <main className="flex-1">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/quiz" element={<Quiz />} />
             <Route path="/result" element={<Result />} />
           </Routes>
         </main>
+
+        <footer className="mt-4 text-center text-gray-600 text-sm border-t border-gray-800/50 pt-4 pb-4">
+            <p className="font-medium text-gray-500 mb-2 cursor-pointer" onClick={() => window.open("https://github.com/vocaloid2048/ARFC", "_blank")}>ARFC 小組專題性格測驗網站</p>
+            <p className="cursor-pointer" onClick={() => window.open("https://github.com/Vocaloid2048")}>&copy; 2026 Vocaloid2048 版權所有</p>
+
+        </footer>
       </div>
     </Router>
   );
